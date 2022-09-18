d45c1d4e172d977ee8fca043e3705f8001e2f5a3
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2022-09-18 18:34:50 +0200

    h

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import FunctionContextComponent from './FunctionContextComponent';
 import './App.css';
 import React, { useState } from 'react'
 export const ThemeContext = React.createContext()
-function App() {
-  const [darkTheme, setDarkTheme] = useState(true)
 
+function App() {
+
+  const [darkTheme, setDarkTheme] = useState(true)
+  const [redTheme, setRedTheme] = useState(true)
+  console.log("darkTheme" + darkTheme)
+  console.log("redTheme" + redTheme)
   function toggleTheme() {
-    setDarkTheme(prevDarkTheme => !prevDarkTheme)
+    setDarkTheme(prevDarkTheme => !prevDarkTheme);
+    console.log("darkTheme" + darkTheme)
+  }
+
+  function textTheme() {
+    setRedTheme(prevRedTheme => !prevRedTheme);
+    console.log("redTheme" + redTheme)
   }
 
   return (
     <div className="App">
-      <ThemeContext.Provider value={darkTheme}>
+    {/* <ThemeContext.Provider value={[darkTheme, redTheme]}> */}
+    <ThemeContext.Provider value={{darkTheme, redTheme}}>
         <button onClick={toggleTheme}>Toggle Theme</button>
+        <button onClick={textTheme}>Text Theme</button>
         <FunctionContextComponent />
-        {/* <ClassContextComponent /> */}
       </ThemeContext.Provider>
     </div>
   );
--- src/FunctionContextComponent.js
+++ src/FunctionContextComponent.js
@@ -2,18 +2,24 @@ import React, { useContext } from 'react'
 import { ThemeContext } from './App'
 
 export default function FunctionContextComponent() {
-  const darkTheme = useContext(ThemeContext)
+
+  const {darkTheme, redTheme} = useContext(ThemeContext)
   const themeStyles = {
     backgroundColor: darkTheme ? '#333' : '#CCC',
     color: darkTheme ? 'beige' : '#333',
     padding: '2rem',
     margin: '2rem',
   }
+  const themeId = {
+    color: redTheme ? 'red' : 'gold',
+  }
 
   return (
-    <div style={themeStyles}>
+    <div style={themeStyles} 
+    id={JSON.stringify(themeId)}>
       Hello World
     </div>
   )
 }
 
+// https://progressivewebninja.com/how-to-pass-multiple-values-in-react-context/
